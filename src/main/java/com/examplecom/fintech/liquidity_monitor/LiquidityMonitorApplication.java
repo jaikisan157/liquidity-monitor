6ec6fd242da36009242f5673f9e06722afcf2892
Jaikisan <JaikisanJegadeesan01@gmail.com>
2026-01-13 09:00:00 +0530

    Initial Spring Boot setup for liquidity monitoring platform

---
+++ src/main/java/com/examplecom/fintech/liquidity_monitor/LiquidityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.examplecom.fintech.liquidity_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidityMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplecom/fintech/liquidity_monitor/LiquidityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplecom.fintech.liquidity_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
